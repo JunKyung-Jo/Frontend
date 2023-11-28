996d720a04b7441ebd28e4478f1a4e9bfef72e4c
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-11-28 14:48:27 +0900

    fix : provider 작동안하던 버그 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Provider from "@/components/common/provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }
