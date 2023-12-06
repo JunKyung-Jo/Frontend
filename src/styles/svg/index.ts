e799f839d0c88c6f2a83303d70a200d18ad7dfb1
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-12-07 00:26:49 +0900

    feat : 아이콘 추가

--- src/styles/svg/index.ts
+++ src/styles/svg/index.ts
@@ -2,3 +2,5 @@ export { default as Logo } from "@/styles/svg/logo";
 export { default as GoogleIcon } from "@/styles/svg/googleIcon";
 export { default as KakaoIcon } from "@/styles/svg/kakaoIcon";
 export { default as NaverIcon } from "@/styles/svg/naverIcon";
+export { default as ArrowIcon } from "@/styles/svg/arrowIcon";
+export { default as CrossIcon } from "@/styles/svg/crossIcon";
