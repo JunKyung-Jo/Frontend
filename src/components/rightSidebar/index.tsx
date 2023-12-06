94ef44f56c472ebbbffa1e9d14a63da85dd253ed
NameIsNick <woosoungcho@gmail.com>
2023-12-07 04:45:26 +0900

    SET:: bot profile

--- src/components/rightSidebar/index.tsx
+++ src/components/rightSidebar/index.tsx
@@ -1,10 +1,25 @@
 import color from "@/styles/theme/color";
-import { Button, Column, Text } from "@/styles/ui";
+import { Button, Column, Row, Text } from "@/styles/ui";
 import styled from "styled-components";
 import { Color, Font } from "@/styles/theme";
+import font from "@/styles/theme/font";
+import { Bluebadge } from "@/styles/svg";
 
 const RightSideBar = () => {
-  return <SideBarPage></SideBarPage>;
+  return (
+    <SideBarPage>
+      <Row>
+        <BotProfile />
+        <div>
+          <Row alignItems="center" gap={0.3} justifyContent="center">
+            <BotName>공지봇</BotName>
+            <Bluebadge width={3} height={3} />
+          </Row>
+          <BotInfo>공지사항봇 입니다</BotInfo>
+        </div>
+      </Row>
+    </SideBarPage>
+  );
 };
 
 export default RightSideBar;
@@ -17,4 +32,24 @@ const SideBarPage = styled.div`
   flex-shrink: 0;
   border-left: 1px solid ${Color.gray200};
   background: ${Color.gray50};
+`;
+
+const BotProfile = styled.div`
+  width: 12.5rem;
+  height: 12.5rem;
+  flex-shrink: 0;
+  border-radius: 50%;
+  background: ${Color.black};
+`;
+
+const BotName = styled.div`
+  color: $H4{Color.black};
+
+  ${font.$H4}
+`;
+
+const BotInfo = styled.div`
+  color: ${Color.black};
+
+  ${font.$Body3}
 `;
--- src/styles/svg/index.ts
+++ src/styles/svg/index.ts
@@ -1,4 +1,5 @@
-export { default as Logo } from "@/styles/svg/logo";
+export { default as Logo } from "@/styles/svg/Logo";
 export { default as GoogleIcon } from "@/styles/svg/googleIcon";
 export { default as KakaoIcon } from "@/styles/svg/kakaoIcon";
 export { default as NaverIcon } from "@/styles/svg/naverIcon";
+export { default as Bluebadge } from "@/styles/svg/bluebadge";
