3b617f85c325c99c887cdf434c0a2c27dce12cf1
NameIsNick <woosoungcho@gmail.com>
2023-12-07 08:22:16 +0900

    SET:: 퍼블리싱

--- src/components/rightSidebar/index.tsx
+++ src/components/rightSidebar/index.tsx
@@ -8,16 +8,25 @@ import { Bluebadge } from "@/styles/svg";
 const RightSideBar = () => {
   return (
     <SideBarPage>
-      <Row>
-        <BotProfile />
-        <div>
-          <Row alignItems="center" gap={0.3} justifyContent="center">
-            <BotName>공지봇</BotName>
-            <Bluebadge width={3} height={3} />
-          </Row>
-          <BotInfo>공지사항봇 입니다</BotInfo>
-        </div>
-      </Row>
+      <RightSidebarHeader>
+        <Row alignItems="center" gap={4.5}>
+          <BotProfile />
+          <div>
+            <Row gap={0.3} alignItems="center">
+              <BotName>공지봇</BotName>
+              <Bluebadge width={3} height={3} />
+            </Row>
+            <BotInfo>이것은 상태 메세지입니다.</BotInfo>
+          </div>
+        </Row>
+        <PostCount>게시물 NN</PostCount>
+      </RightSidebarHeader>
+      <PostContainer>
+        <PostContent>this is a pen</PostContent>
+        <PostContent></PostContent>
+        <PostContent></PostContent>
+        <PostContent></PostContent>
+      </PostContainer>
     </SideBarPage>
   );
 };
@@ -32,6 +41,13 @@ const SideBarPage = styled.div`
   flex-shrink: 0;
   border-left: 1px solid ${Color.gray200};
   background: ${Color.gray50};
+`;
+
+const RightSidebarHeader = styled.div`
+  width: 100%;
+  padding-top: 4.5rem;
+  padding-left: 5.8rem;
+  padding-right: 2.5rem;
 `;
 
 const BotProfile = styled.div`
@@ -52,4 +68,29 @@ const BotInfo = styled.div`
   color: ${Color.black};
 
   ${font.$Body3}
+`;
+
+const PostCount = styled.div`
+  color: ${Color.black};
+  text-align: end;
+  margin-top: 10rem;
+
+  ${font.$Body3}
+`;
+
+const PostContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+const PostContent = styled.div`
+  width: 18rem;
+  height: 18rem;
+  background: ${Color.black};
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  color: ${Color.white};
 `;
