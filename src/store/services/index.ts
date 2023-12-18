1f17cc5887c9ef3f5c53670e01f8b0e65449d96c
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-12-19 03:01:25 +0900

    feat : api할때 쓰는 전역변수선언

---
+++ src/store/services/index.ts
@@ -0,0 +1,11 @@
+import { atom } from "recoil";
+
+export const AImessage = atom({
+  key: "AImessage",
+  default: "",
+});
+
+export const userDataAtom = atom({
+  key: "userData",
+  default: {},
+});
