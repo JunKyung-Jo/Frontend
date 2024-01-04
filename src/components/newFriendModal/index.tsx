4b90b4ae53875186d0ea875e30943f85eba5fb56
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-01-04 20:20:03 +0900

    퍼블리싱

--- src/components/newFriendModal/index.tsx
+++ src/components/newFriendModal/index.tsx
@@ -1,5 +1,7 @@
 import { Text } from "@/styles/ui";
 import * as S from "./style";
+import CheckBox from "@/styles/ui/CheckBox";
+import { CrossIcon } from "@/styles/svg";
 
 interface Modal {
   closeMyModal(): void;
@@ -8,20 +10,30 @@ interface Modal {
 const NewFriendModal = ({ closeMyModal }: Modal) => {
   return (
     <S.Container>
-      <Text fontType="$Body1">새 친구 만나기</Text>
+      <S.CloseModalBtn onClick={closeMyModal}>
+        <CrossIcon width={2} height={2} />
+      </S.CloseModalBtn>
+      <Text fontType="$Body2">새 친구 만나기</Text>
       <S.Contents>
         <S.Content>
-          <Text fontType="$Body2">친구 이름</Text>
+          <Text fontType="$p1">친구 이름</Text>
           <S.Input placeholder="친구 이름을 입력해주세요." />
         </S.Content>
         <S.Content>
-          <Text fontType="$Body2">상태메세지</Text>
+          <Text fontType="$p1">상태메세지</Text>
           <S.Input placeholder="친구의 상태메세지를 입력해주세요." />
         </S.Content>
         <S.Content>
-          <Text fontType="$Body2">친구의 성격</Text>
+          <Text fontType="$p1">친구의 성격</Text>
         </S.Content>
+        <S.botRadioGrid>
+          <CheckBox labels={["외향적인", "내향적인"]} />
+          <CheckBox labels={["현실적인", "창의적인"]} />
+          <CheckBox labels={["계획적인", "자유분방한"]} />
+          <CheckBox labels={["감성적인", "이성적인"]} />
+        </S.botRadioGrid>
       </S.Contents>
+      <S.MakeFriendBtn> 친구 만나기</S.MakeFriendBtn>
     </S.Container>
   );
 };
--- src/components/newFriendModal/style.ts
+++ src/components/newFriendModal/style.ts
@@ -1,8 +1,9 @@
 import color from "@/styles/theme/color";
+import font from "@/styles/theme/font";
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 1000px;
+  width: 600px;
   height: 700px;
   background-color: ${color.white};
   border-radius: 10px;
@@ -21,13 +22,78 @@ export const Contents = styled.div`
 
 export const Content = styled.div`
   margin-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 `;
 
 export const Input = styled.input`
   width: 100%;
-  height: 60px;
-  border: 1px solid ${color.gray100};
+  height: 50px;
+  border: 1.25px solid ${color.gray400};
   border-radius: 10px;
   padding-left: 10px;
   box-sizing: border-box;
+  transition: 0.15s ease-in-out;
+
+  &:focus {
+    border: 1.25px solid ${color.black};
+  }
+`;
+
+export const botRadioGrid = styled.div`
+  min-width: 3rem;
+  width: fit-content;
+  height: fit-content;
+
+  padding: 2rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const CloseModalBtn = styled.div`
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+
+  border-radius: 999px;
+  cursor: pointer;
+
+  padding: 0.5rem;
+  color: ${color.gray200};
+  transition: 0.25s cubic-bezier(0.3, 0.49, 0.5, 1);
+
+  &:hover {
+    background-color: ${color.gray50};
+  }
+
+  &:active {
+    transition: 0.1s cubic-bezier(0.3, 0.49, 0.5, 1);
+    background-color: ${color.gray50};
+  }
+`;
+
+export const MakeFriendBtn = styled.div`
+  width: 140px;
+  height: 50px;
+
+  background-color: ${color.primary};
+  color: white;
+  ${font.$Button4};
+
+  border-radius: 12px;
+  margin-top: 30px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+  transition: 0.15s ease-in-out;
+
+  &:hover {
+    opacity: 0.9;
+  }
 `;
