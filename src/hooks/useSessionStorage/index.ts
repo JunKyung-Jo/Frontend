11d4bcf17a84fadc304d51950b2af3c52c42b201
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2024-01-08 22:51:20 +0900

    wefewf

--- src/hooks/useSessionStorage/index.ts
+++ src/hooks/useSessionStorage/index.ts
@@ -1,27 +1,27 @@
 export const useLocalStorage = () => {
   const getStorageItem = (key: string) => {
-    if (window) {
+    if (typeof window !== "undefined") {
       return window.localStorage.getItem(key);
     }
     return null;
   };
 
   const setStorageItem = (key: string, value: any) => {
-    if (window) {
+    if (typeof window !== "undefined") {
       window.localStorage.setItem(key, value);
     }
     return;
   };
 
   const removeStorageItem = (key: string) => {
-    if (window) {
+    if (typeof window !== "undefined") {
       window.localStorage.removeItem(key);
     }
     return;
   };
 
   const clearStorageItem = () => {
-    if (window) {
+    if (typeof window !== "undefined") {
       window.localStorage.clear();
     }
     return;
