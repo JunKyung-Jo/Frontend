0a8dcc0286dde5547a6ecd44c22672f9e3c03546
lheesung <ihuiseong@ihuiseong-ui-noteubug.local>
2024-01-08 10:21:41 +0900

    chore :: useModal 주석 및 사용법

--- src/components/modal/View.tsx
+++ src/components/modal/View.tsx
@@ -1,3 +1,4 @@
+// ModalView 컴포넌트는 모달의 내용과 배경을 렌더링한다.
 import IModalState from "@/types/hooks/ImodalState";
 import * as S from "./style";
 
@@ -8,6 +9,7 @@ interface ModalViewProps extends IModalState {
 const ModalView = ({ component, visible, onClose }: ModalViewProps) => {
   return (
     <S.ModalContainer>
+      {/* 배경을 클릭하면 모달이 닫히도록 설정 */}
       <S.ModalBackground hidden={!visible} onClick={onClose} />
       <S.ModalBox>{component}</S.ModalBox>
     </S.ModalContainer>
